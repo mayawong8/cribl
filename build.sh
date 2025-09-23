8e66f4c4e24313f0999c4a96643ad653fd850ef0
Maya <mayajwong8@gmail.com>
2025-09-23 14:43:07 -0400

    included clean test output

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 set -e #exit if any commands fail
 docker compose --profile=app up -d --build #start app in the background
-docker compose --profile=tests up --build --exit-code-from tests #exit code after test suite completes
+docker compose run --rm --build tests pytest -v
+#docker compose --profile=tests up --build --exit-code-from tests #exit code after test suite completes
 docker compose --profile=tests --profile=app down #clean up
